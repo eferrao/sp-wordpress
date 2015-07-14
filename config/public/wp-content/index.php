c5e51f2fa38b0750ef435418a15c7959a5fb002d
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-14 10:10:00 -0400

    attempting to add a user into Stylepulse article

--- config/public/wp-content/index.php
+++ config/public/wp-content/index.php
@@ -1,10 +1,26 @@
-<? php get_header(); ?> 
+<? php get_header(); ?>
 
-<div> 
-	<?php while():?> 
+if (have_posts()) :      while (have_posts()) : the_post(); ?>
+
+    <article class = "post">                   
+    	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?>
+    	</a></h2>
+		
+		<font color = #FAED39> 
+		<p class ="post-info"><?php the_time('F j, Y g:i a'); ?> | by <a
+		href="<?php echo get_author_posts_url(get_the_author_meta("ID")); ?>"><?php the_author();
+
+		?></a></p></font> 
+
+        <?php the_content(); ?>      
+    </article>
 
 	<?php endwhile;?>
-</div> 
 
-<? php get_footer(); ?> 
-<? php get_sidebar(); ?>
+	else :  echo '<p>No content found</p>';
+
+	endif;
+	
+
+
+<? php get_footer(); ?>  php get_sidebar(); ?>
