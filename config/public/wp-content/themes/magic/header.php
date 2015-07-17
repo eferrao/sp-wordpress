1b20093f566a68b60f0ffdb9f0822c67444b316a
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-17 09:31:15 -0400

    adding theme #3

---
+++ config/public/wp-content/themes/magic/header.php
@@ -0,0 +1,14 @@
+<!doctype html> 
+<html> 
+<head> 
+	<meta charset = "utf 8" /> 
+	<title><?php bloginfo('title')?></title> 
+	<link rel="stylesheet" href=<?php bloginfo('stylesheet_url')?> /> 
+	<?php up_head()?> 
+</head> 
+
+<header>
+	<h1><a href="<?php home_url('/')?>"><?php bloginfo('name')?></a></h1>
+</header>
+
+<div id = "container">
