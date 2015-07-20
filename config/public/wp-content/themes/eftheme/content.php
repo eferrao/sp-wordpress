06a08c1d739872336869e6af2887923f72a041b2
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-20 13:33:23 -0400

    dfksdl

--- config/public/wp-content/themes/eftheme/content.php
+++ config/public/wp-content/themes/eftheme/content.php
@@ -12,10 +12,18 @@
 		<header class="entry-header">
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
-					<h3 class="cat-links">
-						<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
-						$show_sep = true; ?>
-					</h3>
+					<?php $show_sep = false; ?>
+			<?php if ( is_object_in_taxonomy( get_post_type(), 'category' ) ) : // Hide category text when not supported ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
+				if ( $categories_list ):
+			?>
+			<span class="cat-links">
+				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+				$show_sep = true; ?>
+			</span>
+					
 					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
 				</hgroup>
@@ -50,7 +58,10 @@
 				$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
 				if ( $categories_list ):
 			?>
-
+			<span class="cat-links">
+				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+				$show_sep = true; ?>
+			</span>
 			<?php endif; // End if categories ?>
 			<?php endif; // End if is_object_in_taxonomy( get_post_type(), 'category' ) ?>
 			<?php if ( is_object_in_taxonomy( get_post_type(), 'post_tag' ) ) : // Hide tag text when not supported ?>
