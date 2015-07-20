af5ef585d51e822669fea0aae10ba3e81643a2e2
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-20 15:59:05 -0400

    ajdkslfjds

--- config/public/wp-content/themes/eftheme/content.php
+++ config/public/wp-content/themes/eftheme/content.php
@@ -12,8 +12,24 @@
 		<header class="entry-header">
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
+					<div class = "entry-footer">
+						<?php if ( is_object_in_taxonomy( get_post_type(), 'category' ) ) : // Hide category text when not supported ?>
+						<?php
+							/* translators: used between list items, there is a space after the comma */
+							$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
+							if ( $categories_list ):
+						?>
+						<span class="cat-links">
+							<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+							$show_sep = true; ?>
+						</span>
+					</div>
 					<?php wp_get_post_categories( $post_id, $args ); ?> 
 					<h1> Yes world</h1> 
+					<span class="cat-links">
+						<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
+						$show_sep = true; ?>
+					</span>
 					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
 				</hgroup>
@@ -39,6 +55,8 @@
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
 		<?php endif; ?>
+
+		
 
 		<footer class="entry-meta">
 			<?php $show_sep = false; ?>
