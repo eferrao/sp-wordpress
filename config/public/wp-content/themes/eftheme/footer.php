9a915d37047de4fa8c66f20ca1b2478e583d658b
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-20 12:19:37 -0400

    removed footer text

--- config/public/wp-content/themes/eftheme/footer.php
+++ config/public/wp-content/themes/eftheme/footer.php
@@ -14,22 +14,9 @@
 
 	<footer id="colophon" role="contentinfo">
 
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				if ( ! is_404() )
-					get_sidebar( 'footer' );
-			?>
 
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
-<?php wp_footer(); ?>
 
 </body>
 </html>
