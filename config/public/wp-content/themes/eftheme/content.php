ab2be5a81785cc7955fc876bf1a5c5b99fb5bd21
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-23 11:43:17 -0400

    put back the search part

--- config/public/wp-content/themes/eftheme/content.php
+++ config/public/wp-content/themes/eftheme/content.php
@@ -15,8 +15,7 @@
 			$homepage = "this is home"; 
 			$frontpage = "this is front page"; 
 			if is_home {echo "<h2>$homepage</h2>";} 
-			if is_front_page{echo "h2>$frontpage</h2>";} 
-			?> 
+			if is_front_page{echo "h2>$frontpage</h2>";} ?> 
 
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
@@ -25,12 +24,25 @@
 					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
 				</hgroup>
 			<?php else : ?>
+			
 			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 			<?php endif; ?>
 
 
 
 		</header><!-- .entry-header -->
+
+		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
+		<?php else : ?>
+		<div class="entry-content">
+			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+		<?php endif; ?>
+
 
 
 		<footer class="entry-meta">
