42a778e79dc63a3857f4f9252a89c9b7091b042f
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-23 11:29:34 -0400

    sdfjkldsj

--- config/public/wp-content/themes/eftheme/content.php
+++ config/public/wp-content/themes/eftheme/content.php
@@ -14,7 +14,6 @@
 			<?php if ( is_sticky() ) : ?>
 				<hgroup>
 					<?php wp_get_post_categories( $post_id, $args ); ?> 
-					<h1> Yes world</h1> 
 					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
 				</hgroup>
@@ -26,16 +25,7 @@
 
 		</header><!-- .entry-header -->
 
-		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		<?php else : ?>
-		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<?php endif; ?>
+		
 
 		<footer class="entry-meta">
 			<?php $show_sep = false; ?>
