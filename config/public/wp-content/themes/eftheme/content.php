eb9550c352a1d3ca83736a15f0fd53586ecfd207
Elizabeth Ferrao <ferrao.elizabeth@gmail.com>
2015-07-23 14:47:05 -0400

    all files now look like the image file does

--- config/public/wp-content/themes/eftheme/content.php
+++ config/public/wp-content/themes/eftheme/content.php
@@ -1,86 +1,69 @@
 <?php
 /**
- * The default template for displaying content
+ * The template for displaying posts in the Image Post Format on index and archive pages
+ *
+ * Learn more: http://codex.wordpress.org/Post_Formats
  *
  * @package WordPress
  * @subpackage Twenty_Eleven
  * @since Twenty Eleven 1.0
  */
 ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'indexed' ); ?>>
+		<header class="entry-header elifer4">
+			<hgroup>
+				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+				<h3 class="entry-format"><?php _e( 'Image', 'twentyeleven' ); ?></h3>
+			</hgroup>
 
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header elifer6">
-			<?php if ( is_sticky() ) : ?>
-				<hgroup>
-					<?php wp_get_post_categories( $post_id, $args ); ?> 
-					<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-					<h3 class="entry-format"><?php _e( 'Featured', 'twentyeleven' ); ?></h3>
-				</hgroup>
-			<?php else : ?>
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<?php if ( comments_open() && ! post_password_required() ) : ?>
+			<div class="comments-link">
+				<?php comments_popup_link( '<span class="litwol3 leave-reply">' . __( "Reply", 'twentyeleven' ) . '</span>', _x( '1', 'comments number', 'twentyeleven' ), _x( '%', 'comments number', 'twentyeleven' ) ); ?>
+			</div>
 			<?php endif; ?>
-
-
-
 		</header><!-- .entry-header -->
 
-		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		<?php else : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
 		</div><!-- .entry-content -->
-		<?php endif; ?>
 
 		<footer class="entry-meta">
-			<?php $show_sep = false; ?>
-			<?php if ( is_object_in_taxonomy( get_post_type(), 'category' ) ) : // Hide category text when not supported ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
-				if ( $categories_list ):
-			?>
-			<span class="cat-links">
-				<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list );
-				$show_sep = true; ?>
-			</span>
-			<?php endif; // End if categories ?>
-			<?php endif; // End if is_object_in_taxonomy( get_post_type(), 'category' ) ?>
-			
+			<div class="entry-meta">
+				<?php
+					printf( __( '<a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s">%3$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s" rel="author">%6$s</a></span></span>', 'twentyeleven' ),
+						esc_url( get_permalink() ),
+						get_the_date( 'c' ),
+						get_the_date(),
+						esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+						esc_attr( sprintf( __( 'View all posts by %s', 'twentyeleven' ), get_the_author() ) ),
+						get_the_author()
+					);
+				?>
+			</div><!-- .entry-meta -->
+			<div class="entry-meta">
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
+					if ( $categories_list ):
+				?>
+				<span class="cat-links">
+					<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-cat-links', $categories_list ); ?>
+				</span>
+				<?php endif; // End if categories ?>
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$tags_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
+					if ( $tags_list ): ?>
+				<span class="tag-links">
+					<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+				</span>
+				<?php endif; // End if $tags_list ?>
 
-			<?php if ( comments_open() && ! post_password_required() ) : ?>
-			<div class="comments-link" id = "comments">
-				<?php comments_popup_link( '<span class="litwol1 leave-reply">' . __( 'Reply', 'twentyeleven' ) . '</span>', _x( '1', 'comments number', 'twentyeleven' ), _x( '%', 'comments number', 'twentyeleven' ) ); ?>
-			</div>
-			<?php endif; ?>
-
-
-
-			<?php if ( is_object_in_taxonomy( get_post_type(), 'post_tag' ) ) : // Hide tag text when not supported ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
-				if ( $tags_list ):
-				if ( $show_sep ) : ?>
-			<span class="sep"> | </span>
-				<?php endif; // End if $show_sep ?>
-			<span class="tag-links">
-				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
-				$show_sep = true; ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-			<?php endif; // End if is_object_in_taxonomy( get_post_type(), 'post_tag' ) ?>
-
-			<?php if ( comments_open() ) : ?>
-			<?php if ( $show_sep ) : ?>
-			<span class="sep"> | </span>
-			<?php endif; // End if $show_sep ?>
-			<span class="comments-link"><?php comments_popup_link( '<span class="litwol2 leave-reply">' . __( 'Leave a reply', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'twentyeleven' ), __( '<b>%</b> Replies', 'twentyeleven' ) ); ?></span>
-			<?php endif; // End if comments_open() ?>
+				<?php if ( comments_open() ) : ?>
+				<span class="comments-link"><?php comments_popup_link( '<span class="litwol4 leave-reply">' . __( 'Leave a reply', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'twentyeleven' ), __( '<b>%</b> Replies', 'twentyeleven' ) ); ?></span>
+				<?php endif; // End if comments_open() ?>
+			</div><!-- .entry-meta -->
 
 			<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-meta -->
